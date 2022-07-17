1def50132cf20e142475b688be50820ee8eabb1e
Martin Biström <martin@bistrom.eu>
2022-07-17 22:52:41 +0200

    added package installation to boostrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
+echo "Pulling the latest version of the repo"
 git pull
 
+echo "Creating symlinks for dotfiles"
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
+echo "installing packages"
+dnf install zsh util-linux-user
+
+echo "Changing default shell to ZSH"
+chsh -s $(which zsh)
