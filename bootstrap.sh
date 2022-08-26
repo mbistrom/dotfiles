9bbb06d501d73433548bbd03aa0ac1585d821169
Martin Biström <mbistrom@users.noreply.github.com>
2022-08-26 23:36:22 +0200

    color fux with macos

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,53 +2,59 @@ RED='\033[0;31m' # Red
 GREEN='\033[0;32m' # Green
 NC='\033[0m' # No Color
 
-echo -e "${RED}### ${GREEN}Pulling the latest version of the repo${NC}"
+if [[ $OSTYPE == 'darwin'* ]]; then
+   ESCAPE=""
+else
+   ESCAPE="-e"
+fi
+
+echo ${ESCAPE} "${RED}### ${GREEN}Pulling the latest version of the repo${NC}"
 cd ~/.dotfiles
 git pull
 
-echo -e "${RED}### ${GREEN}Creating necessary folders${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Creating necessary folders${NC}"
 mkdir ~/.local
 mkdir ~/.local/bin
 mkdir ~/.local/share
 
-echo -e "${RED}### ${GREEN}Creating symlinks for dotfiles${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Creating symlinks for dotfiles${NC}"
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.local/bin/update-zsh-plugins ~/.local/bin/update-zsh-plugins
 
-#echo -e "${RED}### ${GREEN}Installing packages${NC}"
+#echo ${ESCAPE} "${RED}### ${GREEN}Installing packages${NC}"
 #sudo dnf -y install git zsh util-linux-user
 
-echo -e "${RED}### ${GREEN}Installing dependencies (git, zsh & chsh)${NC}"
-echo -e "${RED}### ${GREEN}Detecting package manager${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Installing dependencies (git, zsh & chsh)${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Detecting package manager${NC}"
 
 if VERB="$( which apt-get )" 2> /dev/null; then
-   echo -e "${RED}### ${GREEN}Debian-based distro detected - Using APT${NC}"
+   echo ${ESCAPE} "${RED}### ${GREEN}Debian-based distro detected - Using APT${NC}"
    sudo apt-get -y install git zsh
 elif VERB="$( which yum )" 2> /dev/null; then
-   echo -e "${RED}### ${GREEN}Modern Red Hat-based distro detected - Using Yum${NC}"
+   echo ${ESCAPE} "${RED}### ${GREEN}Modern Red Hat-based distro detected - Using Yum${NC}"
    sudo yum -y install git zsh util-linux-user
 elif [[ $OSTYPE == 'darwin'* ]]; then
-   echo -e "${RED}### ${GREEN}MacOS detected - Checking for Homebrew${NC}"
+   echo ${ESCAPE} "${RED}### ${GREEN}MacOS detected - Checking for Homebrew${NC}"
    if VERB="$( which brew )" 2> /dev/null; then
-       echo -e "${RED}### ${GREEN}Using Homebrew${NC}"
+       echo ${ESCAPE} "${RED}### ${GREEN}Using Homebrew${NC}"
        brew install git
    else
-       echo -e "${RED}### Homebrew not found - Please install it!${NC}"
+       echo ${ESCAPE} "${RED}### Homebrew not found - Please install it!${NC}"
        exit 1
    fi
 else
-   echo -e "${RED}### No package manager detected - Cannot install dependencies - Exiting${NC}" >&2
+   echo ${ESCAPE} "${RED}### No package manager detected - Cannot install dependencies - Exiting${NC}" >&2
    exit 1
 fi
 
-echo -e "${RED}### ${GREEN}Installing/updating ZSH plugins${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Installing/updating ZSH plugins${NC}"
 
 ~/.local/bin/update-zsh-plugins
 
-echo -e "${RED}### ${GREEN}Changing default shell to ZSH${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Changing default shell to ZSH${NC}"
 sudo chsh -s $(which zsh) $(whoami)
 
-echo -e "${RED}### ${GREEN}Starting ZSH...${NC}"
+echo ${ESCAPE} "${RED}### ${GREEN}Starting ZSH...${NC}"
 
 zsh
