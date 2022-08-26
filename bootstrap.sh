71bb0cc8162e5fd3160e5a253e2b949f993bbbf2
Martin Biström <mbistrom@users.noreply.github.com>
2022-08-26 23:57:36 +0200

    Comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,33 @@
+# Set up prompt colors
 RED='\033[0;31m' # Red
 GREEN='\033[0;32m' # Green
 NC='\033[0m' # No Color
 
+# If using MacOS, customize how colors are echoed
 if [[ $OSTYPE == 'darwin'* ]]; then
    ESCAPE=""
 else
    ESCAPE="-e"
 fi
 
+# Change into the .dotfiles directory and update pull the repo
 echo ${ESCAPE} "${RED}### ${GREEN}Pulling the latest version of the repo${NC}"
 cd ~/.dotfiles
 git pull
 
+# Create necessary folders
 echo ${ESCAPE} "${RED}### ${GREEN}Creating necessary folders${NC}"
 mkdir ~/.local
 mkdir ~/.local/bin
 mkdir ~/.local/share
 
+# Create symlinks
 echo ${ESCAPE} "${RED}### ${GREEN}Creating symlinks for dotfiles${NC}"
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.local/bin/update-zsh-plugins ~/.local/bin/update-zsh-plugins
 
-#echo ${ESCAPE} "${RED}### ${GREEN}Installing packages${NC}"
-#sudo dnf -y install git zsh util-linux-user
-
+# Perform Package manager detection and install dependencies
 echo ${ESCAPE} "${RED}### ${GREEN}Installing dependencies (git, zsh & chsh)${NC}"
 echo ${ESCAPE} "${RED}### ${GREEN}Detecting package manager${NC}"
 
@@ -48,13 +51,16 @@ else
    exit 1
 fi
 
+# Use the update-zsh-plugins script to update ZSH plugins
 echo ${ESCAPE} "${RED}### ${GREEN}Installing/updating ZSH plugins${NC}"
 
 ~/.local/bin/update-zsh-plugins
 
+# Change the default shell for the logged in user to ZSH
 echo ${ESCAPE} "${RED}### ${GREEN}Changing default shell to ZSH${NC}"
 sudo chsh -s $(which zsh) $(whoami)
 
+# Start a new instance of ZSH
 echo ${ESCAPE} "${RED}### ${GREEN}Starting ZSH...${NC}"
 
 zsh
