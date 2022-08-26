b41d345bdc3f1dc0fef1e6c35339bc9402fce255
Martin Biström <mbistrom@users.noreply.github.com>
2022-08-26 22:52:15 +0200

    Added Package manager detection

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,10 +11,34 @@ ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.local/bin/update-zsh-plugins ~/.local/bin/update-zsh-plugins
 
-echo "### Installing packages"
-sudo dnf -y install git zsh util-linux-user
+#echo "### Installing packages"
+#sudo dnf -y install git zsh util-linux-user
 
-echo "installing/updating ZSH plugins"
+echo "### Installing dependencies (git, zsh & chsh)"
+
+echo "### Detecting package manager"
+
+if VERB="$( which apt-get )" 2> /dev/null; then
+   echo "### Debian-based distro detected - Using APT"
+   sudo apt-get -y install git zsh
+elif VERB="$( which yum )" 2> /dev/null; then
+   echo "### Modern Red Hat-based distro detected - Using Yum"
+   sudo yum -y install git zsh util-linux-user
+elif [[ $OSTYPE == 'darwin'* ]]; then
+   echo "### MacOS detected - Checking for Homebrew"
+   if VERB="$( which brew )" 2> /dev/null; then
+       echo "### Using Homebrew"
+       brew install git
+   else
+       echo "### Homebrew not found - Please install it!"
+   fi
+else
+   echo "### No package manager detected - Cannot install dependencies - Exiting" >&2
+   exit 1
+fi
+
+echo "### Installing/updating ZSH plugins"
+
 ~/.local/bin/update-zsh-plugins
 
 echo "### Changing default shell to ZSH"
